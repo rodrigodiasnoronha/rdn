a7541f958db733bd3cea50c476043939b86fca12
Rodrigo <rodrigo-dias-noronha@outlook.com>
2021-02-16 10:59:53 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from 'next/app';
 import GlobalStyles from '../components/GlobalStyles';
 import ErrorBoundary from '../components/ErrorBoundary';
 import NextNprogress from 'nextjs-progressbar';
+import 'prismjs/components/prism-jsx.min';
 import '../styles/prism-dracula.css';
 import '../styles/global.css';
 import "tailwindcss/tailwind.css";
