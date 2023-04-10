96f7f7ecd0ebe6882d0016722d308383afa9060d
rodrigodiasnoronha <rodrigodnoronha@gmail.com>
2023-04-10 14:37:07 -0300

    erros eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,20 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/eslint-recommended'],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
+    "extends": "plugin:react/recommended",
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 11,
-        sourceType: 'module',
-    },
-    plugins: ['react', '@typescript-eslint'],
-    rules: {
-        'react/prop-types': 0,
-        "no-unused-vars": 0
-    },
-};
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
